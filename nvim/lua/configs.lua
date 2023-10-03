564d4b9855459de04828ce54c87cc8ad80797550
Pavel Sarlov <sarlovpavel@gmail.com>
2023-10-03 19:23:19 +0300

    update telescope

--- nvim/lua/configs.lua
+++ nvim/lua/configs.lua
@@ -81,9 +81,13 @@ if status_ok then
   local actions = require("telescope.actions")
   local actions_layout = require("telescope.actions.layout")
 
-  vim.keymap.set("n", "<c-p>", builtin.find_files, { silent = true, nowait = true, noremap = true })
+  vim.keymap.set("n", "<c-p>", function()
+    local git_root = vim.fn["helpers#FindGitRoot"]()
+    builtin.find_files({ hidden = true, no_ignore = true, cwd = git_root })
+  end, { silent = true, nowait = true, noremap = true })
   vim.keymap.set("n", "<a-S>", function()
-    builtin.grep_string({ word_match = "-w", only_sort_text = true, search = "" })
+    local git_root = vim.fn["helpers#FindGitRoot"]()
+    builtin.grep_string({ word_match = "-w", only_sort_text = true, search = "", cwd = git_root })
   end, { silent = true, nowait = true, noremap = true })
   vim.keymap.set("n", "<c-l>", builtin.buffers, { silent = true, nowait = true, noremap = true })
   vim.keymap.set("n", "<c-g>", builtin.help_tags, { silent = true, nowait = true, noremap = true })
@@ -97,17 +101,18 @@ if status_ok then
           ["<c-s>"] = actions.select_horizontal,
         },
       },
-    },
-    file_ignore_patterns = {
-      ".git/*",
-      ".hg/*",
-      ".svn/*",
-      "node_modules/*",
-      "DS_Store/*",
-      "target/*",
-      "dist/*",
-      "obj/*",
-      "build/*",
+      file_ignore_patterns = {
+        ".git/*",
+        ".hg/*",
+        ".svn/*",
+        "node_modules/*",
+        "DS_Store/*",
+        "target/*",
+        "dist/*",
+        "obj/*",
+        "build/*",
+        "package-lock.json",
+      },
     },
   })
 
