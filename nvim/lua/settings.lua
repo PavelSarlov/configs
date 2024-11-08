a99a023e4bfce0a3822fa6cfeed455b818b73394
Pavel Sarlov <sarlovpavel@gmail.com>
2024-11-08 15:34:38 +0200

    fix global/local options

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -13,37 +13,31 @@ autocmd BufReadPost * set smartindent
 autocmd BufReadPost * set cindent
 
 " autocomplete features
-set omnifunc=syntaxcomplete#complete
+setglobal omnifunc=syntaxcomplete#complete
 set completeopt=menu,menuone,noselect,noinsert
 
 set history=1000
 set encoding=utf-8 fileencoding=utf-8
 set nobackup nowritebackup noswapfile noundofile
 set ignorecase smartcase
-set incsearch
-set hlsearch
-set nrformats-=octal
+set incsearch hlsearch
+setglobal nrformats-=octal
 set title
 set ruler
 set showmode
 set showcmd
-set splitbelow
-set splitright
-set wrap
-set number
-set cursorline
+set splitbelow splitright
+setglobal wrap linebreak number cursorline
 set clipboard+=unnamed,unnamedplus
 
 set laststatus=2
-set tabstop=2
-set softtabstop=2
-set shiftwidth=2
+setglobal tabstop=2 softtabstop=2 shiftwidth=2
 set backspace=indent,eol,start
 
-set expandtab
+setglobal expandtab
 set smarttab
-set autoread
-set virtualedit=all
+setglobal autoread
+setglobal virtualedit=all
 set t_vb=
 set t_u7=
 set novisualbell
@@ -51,10 +45,10 @@ set belloff=all
 set hidden
 set updatetime=300
 set shortmess=acF
-set signcolumn=yes
-set relativenumber
+setglobal signcolumn=yes
+setglobal relativenumber
 
-set undofile
+setglobal undofile
 let &undodir=g:VIMUNDODIR
 
 set timeout
@@ -73,4 +67,5 @@ set wildoptions=pum
 set wildignore+=*\\tmp\\*,*/tmp/*,*.so,*.swp,*.zip,*.exe,*.dll
 
 let g:editorconfig = v:true
+
 ]]
