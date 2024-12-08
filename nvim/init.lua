fa3bb29117e656304217e980cf0895f546c997a6
Pavel Sarlov <sarlovpavel@gmail.com>
2024-12-08 10:03:21 +0200

    use wslyank instead of clip

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,15 +17,18 @@ if vim.fn.has("unix") == 1 then
 
 	vim.g.SLASH = "/"
 
-	vim.cmd([[
-    let s:clip = '/mnt/c/Windows/System32/clip.exe'
-    if executable(s:clip)
-      augroup WSLYank
-        autocmd!
-        autocmd TextYankPost * if v:event.operator ==# 'y' | call system(s:clip, @0) | endif
-      augroup END
-    endif
-  ]])
+	vim.g.clipboard = {
+		name = "win32yank-wsl",
+		copy = {
+			["+"] = "win32yank.exe -i --crlf",
+			["*"] = "win32yank.exe -i --crlf",
+		},
+		paste = {
+			["+"] = "win32yank.exe -o --lf",
+			["*"] = "win32yank.exe -o --lf",
+		},
+		cache_enabled = 0,
+	}
 end
 
 vim.env.VIMHOME = table.concat({ vim.env.HOME, ".nvim" }, vim.g.SLASH)
