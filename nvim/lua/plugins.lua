0b4f3a75beaaf333af2e8b722d2afd1038b479ee
Pavel Sarlov <sarlovpavel@gmail.com>
2026-04-13 18:46:22 +0300

    remove telescope default branch

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -60,7 +60,6 @@ if status then
 
 			{
 				"nvim-telescope/telescope.nvim",
-				branch = "0.1.x",
 				dependencies = {
 					"nvim-lua/plenary.nvim",
 					"nvim-telescope/telescope-fzf-native.nvim",
